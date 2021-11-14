fc1df547ef1fcf3449e0d9a93e9664e127bdfd64
Jiaobuzuji <Jiaobuzuji@163.com>
2021-11-14 23:22:51 +0800

    uninstall nvim

---
+++ tools/uninstaller.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+sudo rm -rf /usr/bin/nvim
+sudo rm -rf /usr/lib/nvim/
+sudo rm -rf /usr/share/applications/nvim.desktop
+sudo rm -rf /usr/share/icons/hicolor/128x128/apps/nvim.png
+sudo rm -rf /usr/share/locale/**/nvim.mo
+sudo rm -rf /usr/share/man/man1/nvim.1
+sudo rm -rf /usr/share/nvim/
