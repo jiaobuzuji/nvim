ce9d86bce9f8c9df53ca7a4b15a654fb29bb5272
Jiaobuzuji <Jiaobuzuji@163.com>
2021-12-14 12:44:29 +0800

    fix stdpaths bug

--- tools/installer.sh
+++ tools/installer.sh
@@ -9,7 +9,7 @@ declare -r XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
 declare -r XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 
 declare -r GITSRVURL="${GITSRVURL:-"github.com.cnpmjs.org"}"  # mirror0: github.com.cnpmjs.org
-declare -r NVIMDATA="${NVIMDATA:-"$XDG_DATA_HOME/nvim-data"}"
+declare -r NVIMDATA="${NVIMDATA:-"$XDG_DATA_HOME/nvim"}"
 
 mkdir -p ${NVIMDATA}/swap
 git clone --depth=1 https://${GITSRVURL}/wbthomason/packer.nvim\
