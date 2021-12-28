9266b3d41e0eb30deca48e2696ef647c6e31aa29
Jiaobuzuji <Jiaobuzuji@163.com>
2021-12-28 13:29:26 +0800

    nvim tree, use default config

--- lua/plugins/awesome.lua
+++ lua/plugins/awesome.lua
@@ -227,47 +227,48 @@ end
 M.nvimtree_setup = function()
   local tree_cb = require'nvim-tree.config'.nvim_tree_callback
   require('nvim-tree').setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-    open_on_setup       = false,
-    ignore_ft_on_setup  = {},
+    -- disable_netrw       = true,
+    -- hijack_netrw        = true,
+    -- open_on_setup       = false,
+    -- ignore_ft_on_setup  = {},
+    -- auto_close          = false,
+    -- open_on_tab         = false,
+    -- hijack_cursor       = false,
+    update_cwd          = true,
     update_to_buf_dir   = {
       enable = true,
       auto_open = true,
     },
-    auto_close          = false,
-    open_on_tab         = false,
-    hijack_cursor       = false,
-    update_cwd          = true,
-    diagnostics         = {
-      enable = false,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      }
-    },
-    update_focused_file = {
-      enable      = false,
-      update_cwd  = true,
-      ignore_list = {}
-    },
-    system_open = {
-      cmd  = nil,
-      args = {}
-    },
-    git = {
-      enable = true,
-      ignore = true,
-    },
+    -- diagnostics         = {
+    --   enable = false,
+    --   icons = {
+    --     hint = "",
+    --     info = "",
+    --     warning = "",
+    --     error = "",
+    --   }
+    -- },
+    -- update_focused_file = {
+    --   enable      = false,
+    --   update_cwd  = true,
+    --   ignore_list = {}
+    -- },
+    -- system_open = {
+    --   cmd  = nil,
+    --   args = {}
+    -- },
+    -- git = {
+    --   enable = true,
+    --   ignore = true,
+    --   timeout = 500,
+    -- },
     view = {
       width = '40%',
       height = 30,
       side = 'left',
-      auto_resize = false,
-      number = false,
-      relativenumber = true,
+      -- auto_resize = false,
+      -- number = false,
+      -- relativenumber = true,
       mappings = {
         custom_only = false,
         list = {
@@ -277,14 +278,14 @@ M.nvimtree_setup = function()
         }
       }
     },
-    filters = {
-      dotfiles = false,
-      custom = {}
-    },
-    trash = {
-      cmd = "trash",
-      require_confirm = true,
-    }
+    -- filters = {
+    --   dotfiles = false,
+    --   custom = {}
+    -- },
+    -- trash = {
+    --   cmd = "trash",
+    --   require_confirm = true,
+    -- }
   }
 end
 
@@ -323,7 +324,7 @@ M.nvimtree = function()
       error = "",
     }
   }
-
+  g.nvim_tree_git_hl = 1
   g.nvim_tree_highlight_opened_files = 1
   g.nvim_tree_quit_on_open = 1
   g.nvim_tree_group_empty = 1
